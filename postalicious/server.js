acd18ee65aeb61980719ee7aca4fb4cf72805667
jason00111 <emberleyjason@gmail.com>
2017-03-13 21:50:43 -0700

    wip

--- postalicious/server.js
+++ postalicious/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.get('/', (req, res) => {
-  
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3001, () => console.log('postalicious server listening on port 3001'))
